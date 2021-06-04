44fd16b1000d8eae866bf5ff00440dd0eaf5ce26
zhuravko <zhuravko@cleverstaff.net>
2021-06-04 16:23:05 +0300

    fix new-car-page

--- src/components/pages/new-car-page.js
+++ src/components/pages/new-car-page.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CarFieldsForm from "../car-fields-form";
+import { useDispatch, useSelector } from "react-redux";
 
+import { getLoading, getError } from "../../store/selectors/cars";
+import { setLoading } from "../../store/actions/cars";
 import { addNewCar } from "../../store/thunks/cars";
 
-
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 const NewCarPage = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector(getLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(setLoading(false));
+  }, [dispatch]);
+
   const initialFormFields = {
     brand: "",
     model: "",
@@ -15,6 +27,14 @@ const NewCarPage = () => {
     maxSpeed: "",
     description: "",
   };
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <ErrorIndicator errorMessage={error} />;
+  }
 
   return (
     <div className="new-car-page-container">
